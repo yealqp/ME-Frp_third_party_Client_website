b45052479f1c2e0ad6b897996fffb0082d94ad54
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2025-10-11 22:45:20 +0800

    emergency(docs): 更改了错误的命令(/hefl/ -> /mefl/)

--- src/components/docs/QYF.vue
+++ src/components/docs/QYF.vue
@@ -149,7 +149,7 @@ onMounted(() => {
           <li>预先安装 .NET 8.0 运行时（参考上文）</li>
           <li>打开终端</li>
           <li>输入
-            <pre><code class="language-bash">bash <(curl -sSL https://content.rycb.mxj.pub/files/hefl/install.sh)</code></pre>
+            <pre><code class="language-bash">bash <(curl -sSL https://content.rycb.mxj.pub/files/mefl/install.sh)</code></pre>
           </li>
           <li>等待安装完成</li>
           <li>之后可直接输入 <code>mefrplauncherx</code> 启动</li>
